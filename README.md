d6a5a724bee8cddc59d1ef46e416312f9adffdfc
ihpolyphe <hayato.8124950@mineo.jp>
2022-06-30 22:20:55 +0900

    For loop in Vue 100%

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue Official Turtorial
+
+I study Vue.js using Vue.js officail site.
+https://jp.vuejs.org/v2/guide/#
+
+This is the best for me to study Vue.js as Handson.
+https://scrimba.com/learn/vuedocs
+
+# Today
+
+I finished until 4, and next I contenue to study from No.5.
--- main.js
+++ main.js
@@ -3,8 +3,14 @@ var app = new Vue({
   data: {
     message: "Hello Vue!",
     seen: true,
+    todos: [
+      { text: "do dualingo for studying English" },
+      { text: "do mikan" },
+      { text: "do TED" },
+    ],
   },
 });
 
 app.message = "I start to study Vue";
 app.seen = false;
+app.todos.push({ text: "Read book of vocabulary" });
